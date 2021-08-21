64a65ef51fd46a7e6d97494cc1dae884212023b2
grey4uk <grey4uk14@gmail.com>
2021-08-21 17:31:41 +0300

    changed config for firebase

--- 01/src/services/config.js
+++ 01/src/services/config.js
@@ -3,13 +3,13 @@ import "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAzpS4wyPyqTIe0gsAB7_pewjjj-9ho7jA",
+  apiKey: `${process.env.API_KEY}`,
   authDomain: "contactsdatabase-26d45.firebaseapp.com",
   projectId: "contactsdatabase-26d45",
   storageBucket: "contactsdatabase-26d45.appspot.com",
-  messagingSenderId: "1083474065616",
-  appId: "1:1083474065616:web:297b55000aeae5e5cb06d5",
-  measurementId: "G-B5Z5D05PHF",
+  messagingSenderId: `${process.env.MESSAGING_SENDER_ID}`,
+  appId: `${process.env.APP_ID}`,
+  measurementId: `${process.env.MEASUREMENT_ID}`,
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
